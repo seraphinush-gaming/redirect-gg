712d8be66553727f383f5f7d974aa44436f8fba2
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-02-25 14:04:36 +0900

    1.23 r:03

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 // S_LOAD_TOPO
 // S_SPAWN_ME
 
-// Version 1.23 r:02
+// Version 1.23 r:03
 
 const GHILLIEGLADE = { x: 52232.98046875, y: 117318.875, z: 4382.390625 }
 
-module.exports = function RedirectGhillieglade(d) {
+module.exports = function RedirectGg(d) {
 
 	let enable = true,
 		myZone = -1
@@ -25,7 +25,9 @@ module.exports = function RedirectGhillieglade(d) {
 			return true
 		} 
 		// auto-reset at Velik's Sanctuary
-		else if (myZone === 9714) d.toServer('C_RESET_ALL_DUNGEON', {})
+		else if (myZone === 9714) {
+			d.toServer('C_RESET_ALL_DUNGEON', {})
+		}
 	})
 
 	// command
@@ -35,10 +37,10 @@ module.exports = function RedirectGhillieglade(d) {
 		// toggle
 		command.add(['gg', 'ㄱㄹ'], () => {
 			enable = !enable
-			send(`${enable ? '실행되었습니다'.clr('56B4E9') : '중지되었습니다'.clr('E69F00')}` + `.`.clr('FFFFFF'))
+			send(`${enable ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')}` + `.`.clr('FFFFFF'))
 		})
-		function send(msg) { command.message(`[redirect-ghillieglade] : ` + [...arguments].join('\n\t - ')) }
-	} catch (e) { console.log(`[ERROR] -- redirect-ghillieglade module --`) }
+		function send(msg) { command.message(`[redirect-gg] : ` + [...arguments].join('\n\t - ')) }
+	} catch (e) { console.log(`[ERROR] -- redirect-gg module --`) }
 
 }
 
