15fa2449ba03a96136a2ca4a24e31cc4b4bf0933
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-03-20 23:43:37 +0900

    1.24 r:00

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
-// OPCODE REQUIRED :
-// C_RESET_ALL_DUNGEON
-// S_LOAD_TOPO
-// S_SPAWN_ME
+// Version 1.24 r:00
 
-// Version 1.23 r:03
+const Command = require('command')
+const config = require('./config.json')
 
 const GHILLIEGLADE = { x: 52232.98046875, y: 117318.875, z: 4382.390625 }
 
-module.exports = function RedirectGg(d) {
+// credit : https://github.com/Some-AV-Popo
+String.prototype.clr = function (hexColor) { return `<font color="#${hexColor}">${this}</font>` }
 
-	let enable = true,
-		myZone = -1
+module.exports = function RedirectGg(d) {
+	const command = Command(d)
+
+	let enable = config.enable
+
+	let	myZone = -1
 
 	// code
-	d.hook('S_LOAD_TOPO', (e) => {
-		myZone = e.zone
-	})
+	d.hook('S_LOAD_TOPO', (e) => { myZone = e.zone })
 
 	d.hook('S_SPAWN_ME', (e) => {
 		if (!enable) return
 		// Ghillieglade dungeon
 		if (myZone === 9713) {
-			Object.assign(e, GHILLIEGLADE)
+			Object.assign(e.loc, GHILLIEGLADE)
 			return true
 		} 
 		// auto-reset at Velik's Sanctuary
@@ -31,18 +32,11 @@ module.exports = function RedirectGg(d) {
 	})
 
 	// command
-	try {
-		const Command = require('command')
-		const command = Command(d)
-		// toggle
-		command.add(['gg', 'ㄱㄹ'], () => {
-			enable = !enable
-			send(`${enable ? 'enabled'.clr('56B4E9') : 'disabled'.clr('E69F00')}` + `.`.clr('FFFFFF'))
-		})
-		function send(msg) { command.message(`[redirect-gg] : ` + [...arguments].join('\n\t - ')) }
-	} catch (e) { console.log(`[ERROR] -- redirect-gg module --`) }
+	// toggle
+	command.add(['gg', 'ㄱㄹ'], () => {
+		enable = !enable
+		send(`${enable ? 'Enabled'.clr('56B4E9') : 'Disabled'.clr('E69F00')}`)
+	})
+	function send(msg) { command.message(`[redirect-gg] : ` + msg) }
 
 }
-
-// credit : https://github.com/Some-AV-Popo
-String.prototype.clr = function (hexColor) { return `<font color="#${hexColor}">${this}</font>` }
