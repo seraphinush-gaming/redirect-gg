b6a57f2d242b95e3e1d1b274af63e86c8074b400
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-05-03 13:30:34 +0900

    1.24 r:03

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-// Version 1.24 r:02
+// Version 1.24 r:03
 
 const Command = require('command')
 const config = require('./config.json')
 
-const GHILLIEGLADE = { x: 52232, y: 117318, z: 4388 }
+const BANKAYAS = { x: 52232, y: 117318, z: 4500 }
+const GHILLIEGLADE = 9713
+const SANCTUARY = 9714
 
 // credit : https://github.com/Some-AV-Popo
 String.prototype.clr = function (hexColor) { return `<font color="#${hexColor}">${this}</font>` }
@@ -21,12 +23,12 @@ module.exports = function RedirectGg(d) {
 	d.hook('S_SPAWN_ME', (e) => {
 		if (!enable) return
 		// Ghillieglade dungeon
-		if (myZone === 9713) {
-			Object.assign(e.loc, GHILLIEGLADE)
+		if (myZone === GHILLIEGLADE) {
+			Object.assign(e.loc, BANKAYAS)
 			return true
 		} 
 		// auto-reset at Velik's Sanctuary
-		else if (myZone === 9714) {
+		else if (myZone === SANCTUARY) {
 			d.toServer('C_RESET_ALL_DUNGEON', {})
 		}
 	})
