8c8bc78846f5ea653de70fbb138af12e2d4610a8
seraph <30970630+seraphinush-gaming@users.noreply.github.com>
2018-09-05 00:22:48 -0700

    1.26 r:01

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-// Version 1.26 r:00
+// Version 1.26 r:01
 
 const config = require('./config.json');
 
-const LOC_BANKAYAS = { x: 52232, y: 117318, z: 4500 };
-const ZONE_GHILLIEGLADE = 9713;
+const location = { 9713: { x: 52232, y: 117318, z: 4500 } };
 const ZONE_SANCTUARY = 9714;
 
 module.exports = function RedirectGg(m) {
@@ -23,14 +22,14 @@ module.exports = function RedirectGg(m) {
 	});
 
 	// mod.game
-	m.game.on('change_zone', (zone, quick) => { myZone = zone; });
+	m.game.me.on('change_zone', (zone, quick) => { myZone = zone; });
 
 	// code
 	m.hook('S_SPAWN_ME', 3, (e) => {
 		if (!enable) return
 		// Ghillieglade dungeon
-		if (myZone === ZONE_GHILLIEGLADE) {
-			Object.assign(e.loc, LOC_BANKAYAS);
+		if (myZone in location) {
+			Object.assign(e.loc, location[myZone]);
 			return true
 		} 
 		// auto-reset at Velik's Sanctuary
